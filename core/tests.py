76b95e8ba0d2fc97ddbacc6f79b7778bb6927cce
Nihar <yakshudave696@gmail.com>
2026-03-18 14:05:36 +0530

    feat: add PainZoneReportForm

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import PainZoneReport, PREDEFINED_ZONES
+from .forms import PainZoneReportForm
 
 User = get_user_model()
 
@@ -30,3 +31,37 @@ class PainZoneReportModelTest(TestCase):
         self.assertEqual(len(PREDEFINED_ZONES), 8)
         self.assertIn('lower_back', PREDEFINED_ZONES)
         self.assertIn('tailbone', PREDEFINED_ZONES)
+
+
+class PainZoneReportFormTest(TestCase):
+    def test_valid_zones_accepted(self):
+        form = PainZoneReportForm(data={
+            'zones': ['lower_back', 'tailbone'],
+            'note': 'hurts',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_zone_rejected(self):
+        form = PainZoneReportForm(data={
+            'zones': ['invented_zone'],
+            'note': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('zones', form.errors)
+
+    def test_empty_zones_rejected(self):
+        form = PainZoneReportForm(data={'zones': [], 'note': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('zones', form.errors)
+
+    def test_note_is_optional(self):
+        form = PainZoneReportForm(data={'zones': ['left_hip']})
+        self.assertTrue(form.is_valid())
+
+    def test_note_max_length(self):
+        form = PainZoneReportForm(data={
+            'zones': ['left_hip'],
+            'note': 'x' * 1001,
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('note', form.errors)
